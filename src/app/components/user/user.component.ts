ab7dea902fcf8a64d1e6f5250700c0d03f4dfba8
lelinrashed <lelin.rashed784@gmail.com>
2018-10-26 01:33:35 +0600

    Push after setup the frontend part

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: string[];
+  hello: any;
+
+  constructor() {}
 
   ngOnInit() {
+    this.name = 'lelinrashed';
+    this.age = 30;
+    this.email = 'lelin.rashed784@gmail.com';
+    this.address = {
+      street: '50 main st',
+      city: 'landon',
+      state: '221/b'
+    };
+    this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
+    this.hello = 20;
   }
 
+  onClick() {
+    this.name = 'Md.Rashedul Islam';
+    this.hobbies.push('New hobby');
+  }
+
+  addHobby(hobby) {
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby) {
+    for (let i = 0; i < this.hobbies.length; i++) {
+      if (this.hobbies[i] === hobby) {
+        this.hobbies.splice(i, 1);
+      }
+    }
+  }
+
+}
+
+
+interface Address {
+  street: string;
+  city: string;
+  state: string;
 }
